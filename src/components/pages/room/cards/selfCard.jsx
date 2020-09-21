d65b8c815e6ce45283a27028203030da6b2b2b93
sastaachar <justinjohnmathew@karunya.edu.in>
2020-09-21 23:07:26 +0530

    [⚡ General update : Stream state] v1 of this feature is complete

--- src/components/pages/room/cards/selfCard.jsx
+++ src/components/pages/room/cards/selfCard.jsx
@@ -3,116 +3,151 @@ import React, { useRef, useState } from "react";
 import MovableCard from "../../../shared/movableCard";
 import { connect } from "react-redux";
 import { useEffect } from "react";
+import { updateStreamState } from "../../../../actions/streamActions";
 
 const SelfCard = (props) => {
   const videoNode = useRef(null);
 
-  const [panner, setPan] = useState(null);
+  //const [panner, setPan] = useState(null);
 
   useEffect(() => {
     if (videoNode.current && props.stream) {
       videoNode.current.srcObject = props.stream;
+      console.log("got new stream");
     }
   }, [props.stream]);
 
-  const startCapture = () => {
-    console.log("awd");
-    const getMedia = navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+  const changeVideoState = () => {
+    const { stream: userStream, streamState } = props;
+    try {
+      if (streamState.video) {
+        // video is on turn it off
+        userStream.getVideoTracks()[0].stop();
+        props.updateStreamState({
+          audio: streamState.audio,
+          video: !streamState.video,
+        });
+      } else {
+        // video is off turn it on
+        const getMedia = navigator.mediaDevices.getUserMedia({
+          audio: streamState.audio,
+          video: true,
+        });
 
-    getMedia.then((stream) => {
-      videoNode.current.srcObject = stream;
-      const AudioContext = window.AudioContext || window.webkitAudioContext;
-      let audioCtx = new AudioContext();
+        getMedia
+          .then((stream) => {
+            // disable all previous streams
+            if (userStream)
+              userStream.getTracks().forEach((track) => {
+                track.stop();
+              });
 
-      const mediaStreamSource = audioCtx.createMediaStreamSource(stream);
-
-      let panner = audioCtx.createStereoPanner();
-      panner.pan.value = 1;
-
-      mediaStreamSource.connect(panner);
-      panner.connect(audioCtx.destination);
-
-      setPan(panner);
-
-      props.setMyStream(stream);
-    });
+            // send every one new stream
+            Object.entries(props.senders).forEach((sender) => {
+              stream.getTracks().forEach((track) => {
+                if (track.kind === sender.track.kind) {
+                  sender
+                    .replaceTrack(track)
+                    .then(() => {
+                      console.log(`Replaced ${track.kind} track`);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                }
+              });
+            });
+            // change stream state
+            props.updateStreamState({
+              audio: streamState.audio,
+              video: !streamState.video,
+            });
+            // need to replace my current stream with new one
+            props.setMyStream(stream);
+          })
+          .catch((e) => {
+            console.log("Could not access your camera or microphone");
+            return;
+          });
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
+  const changeAudioState = () => {
+    const { stream: userStream, streamState } = props;
+    try {
+      if (streamState.audio) {
+        // video is on turn it off
+        userStream.getAudioTracks()[0].stop();
+        // chagne state
+        props.updateStreamState({
+          audio: !streamState.audio,
+          video: streamState.video,
+        });
+      } else {
+        // video is off turn it on
+        const getMedia = navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: streamState.video,
+        });
+        getMedia
+          .then((stream) => {
+            // disable all previous streams
+            userStream.getTracks().forEach((track) => {
+              track.stop();
+            });
 
-  const userStream = props.stream;
+            // send every one new stream
+            Object.entries(props.senders).forEach((sender) => {
+              stream.getTracks().forEach((track) => {
+                if (track.kind === sender.track.kind) {
+                  sender
+                    .replaceTrack(track)
+                    .then(() => {
+                      console.log(`Replaced ${track.kind} track`);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                }
+              });
+            });
+            // chagne state
+            props.updateStreamState({
+              audio: !streamState.audio,
+              video: streamState.video,
+            });
+            // need to replace my current stream with new one
+            props.setMyStream(stream);
+          })
+          .catch((e) => {
+            console.log("Could not access your camera or microphone");
+            return;
+          });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <MovableCard>
       <div className="cardContainer">
         <video playsInline autoPlay ref={videoNode} />
-        <span>this is me {props.userName}</span>
-        <button
-          onClick={() => {
-            userStream.getVideoTracks()[0].enabled = !userStream
-              .getVideoTracks()[0]
-              .stop();
-          }}
-        >
-          mute video
-        </button>
-        <button
-          onClick={() => {
-            const getMedia = navigator.mediaDevices.getUserMedia({
-              audio: false,
-              video: true,
-            });
-
-            getMedia
-              .then((stream) => {
-                const videoTrack = stream.getTracks()[0];
-                console.log("new track ", videoTrack);
-                Object.entries(props.senders).forEach((ele) => {
-                  const senderList = ele[1];
-                  senderList.forEach((sender) => {
-                    if (sender.track.kind === "video") {
-                      sender
-                        .replaceTrack(videoTrack)
-                        .then((e) => {
-                          console.log("video goes brrrrrrrr", e);
-                        })
-                        .catch(() => {
-                          console.log("nai hua lode tera");
-                        });
-                    }
-                  });
-                });
-                props.setMyStream(stream);
-              })
-              .catch((e) => console.log(e));
-          }}
-        >
-          video again
-        </button>
-
-        <button
-          onClick={() => {
-            // userStream.getVideoTracks()[0].load();
-            userStream.getAudioTracks()[0].enabled = !userStream.getAudioTracks()[0]
-              .enabled;
-          }}
-        >
-          mute audio
-        </button>
-        <button onClick={startCapture}>pan</button>
-        <button
-          onClick={() => {
-            panner.pan.value = -1 * panner.pan.value;
-          }}
-        >
-          flip
-        </button>
+        {props.streamState.video ? <span>v</span> : null}
+        {props.streamState.audio ? <span>a</span> : null}
+        <div className="card-btnContainter">
+          <button onClick={changeVideoState}>video</button>
+          <button onClick={changeAudioState}>audio</button>
+        </div>
       </div>
     </MovableCard>
   );
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  streamState: state.streamData,
+});
 
-export default connect(mapStateToProps, null)(SelfCard);
+export default connect(mapStateToProps, { updateStreamState })(SelfCard);
