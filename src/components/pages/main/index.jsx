d199062a0985f915f11416fbd3fd189b9e3ebd9b
sastaachar <justinjohnmathew@karunya.edu.in>
2020-09-26 21:48:25 +0530

    [🎨 Cosmetic : Main page] Basic styling

--- src/components/pages/main/index.jsx
+++ src/components/pages/main/index.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import { getNewRoom } from "../../../actions/roomActions";
+
+import "./style.css";
 
 const MainPage = (props) => {
   const [room_id, setRoomid] = useState("");
@@ -10,6 +13,7 @@ const MainPage = (props) => {
 
   const handleCreateRoom = (e) => {
     // send GET req to server
+    e.preventDefault();
     props.getNewRoom((data) => {
       // get new id and redirect
       setRoomid(data.room_id);
@@ -22,16 +26,28 @@ const MainPage = (props) => {
   }
 
   return (
-    <div className="mainPage-container">
-      <label htmlFor="room_id">Enter Room id</label>
-      <input
-        id="room_id"
-        type="text"
-        onChange={(e) => setRoomid(e.target.value)}
-      />
-      <button onClick={() => setRedirect(true)}> Go </button>
-      {props.roomReqLoading ? <span>Req sent...</span> : null}
-      <button onClick={handleCreateRoom}> Create room </button>
+    <div className="mainPage-wrapper simple-layout">
+      <div className="mainPage-box ">
+        <section className="roomIdInputs">
+          <label htmlFor="room_id">Enter Room id :</label>
+          <div>
+            <input
+              id="room_id"
+              type="text"
+              onChange={(e) => setRoomid(e.target.value)}
+            />
+            <Link to={`/room/${room_id}`} className="simple-btn">
+              go
+            </Link>
+          </div>
+        </section>
+        <section className="createRoom">
+          <a onClick={handleCreateRoom} href="#" className="simple-btn">
+            create room
+          </a>
+          {props.roomReqLoading ? <span>Req sent...</span> : null}
+        </section>
+      </div>
     </div>
   );
 };
