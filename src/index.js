f25a1600f4718e3cf8eb108b7c8bb734a75ba574
sastaachar <sastaachar>
2020-12-15 17:50:40 +0530

    [💥 Breaking changes : Compatibility with github pages] now supporting github pages too.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL_ROUTE || "/vieu"}>
+    <Router basename={"/vieu"}>
       <App />
     </Router>
   </React.StrictMode>,
