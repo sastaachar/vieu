59aad0ec44c7901d1fd1538a3c26835a9b6b93df
sastaachar <sastaachar>
2020-12-17 18:30:02 +0530

    [🔨 Refactor code : Minor Chages] Removed homepage.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={""}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
