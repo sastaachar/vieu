118b9bb2894c908aa798974dbf95b1b46e53cb69
sastaachar <sastaachar>
2020-12-21 18:45:38 +0530

    [🔨 Refactor code : Card Component] One unified card

---
+++ src/components/pages/room/cards/card.jsx
@@ -0,0 +1,70 @@
+import React, { useRef, useEffect } from "react";
+import { connect } from "react-redux";
+
+import MovableCard from "../../../shared/movableCard";
+
+import "./style.css";
+
+const UserCard = (props) => {
+  const userVideoNode = useRef(null);
+  const userAudioNode = useRef(null);
+  const screenVideoNode = useRef(null);
+
+  const {
+    userID,
+    streams: { userVideo, userAudio, screenVideo },
+  } = props;
+
+  useEffect(() => {
+    if (userVideo) {
+      userVideoNode.current.srcObject = userVideo;
+    }
+  }, [userVideo, userID]);
+
+  useEffect(() => {
+    if (userAudio) {
+      console.log("user audio changed for ", userID);
+
+      userAudioNode.current.srcObject = userAudio;
+    }
+  }, [userAudio, userID]);
+
+  useEffect(() => {
+    if (screenVideo) {
+      screenVideoNode.current.srcObject = screenVideo;
+    }
+  }, [screenVideo, userID]);
+
+  return (
+    <MovableCard>
+      <div className="cardContainer">
+        <span>{props.userName}</span>
+        {userVideo && (
+          <video
+            className="user-video"
+            playsInline
+            autoPlay
+            controls
+            ref={userVideoNode}
+          />
+        )}
+        {userAudio && (
+          <audio autoPlay className="user-audio" ref={userAudioNode}></audio>
+        )}
+        {screenVideo && (
+          <video
+            className="screen-video"
+            ref={screenVideo}
+            playsInline
+            autoPlay
+            controls
+          ></video>
+        )}
+      </div>
+    </MovableCard>
+  );
+};
+
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps, null)(UserCard);
